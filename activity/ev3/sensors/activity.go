737e4872e41fece9a589bd390b13a76f60a9b979
magallardo <gallardo@tibco.com>
2018-11-06 13:12:10 -0500

    Create activity.go

---
+++ activity/ev3/sensors/activity.go
@@ -0,0 +1,41 @@
+package EV3Activity
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+// THIS IS ADDED
+// log is the default package logger which we'll use to log
+var log = logger.GetLogger("activity-helloworld")
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+
+	// Get the activity data from the context
+	input := context.GetInput("input").(string)
+
+	// Use the log object to log the greeting
+	log.Infof("The Flogo engine says input is: [%s]", input)
+
+	// Set the result as part of the context
+	context.SetOutput("output", "The Flogo engine says "+input)
+
+	// Signal to the Flogo engine that the activity is completed
+	return true, nil
+}
